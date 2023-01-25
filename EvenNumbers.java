3fed4d363e5cb094e98849c6a5f242d197786c12
Nikhil Sharma <122785600+codewithnikhilsharma@users.noreply.github.com>
2023-01-25 19:00:04 +0530

    Add files via upload

---
+++ EvenNumbers.java
@@ -0,0 +1,10 @@
+       public class EvenNumbers {
+            public static void main(String[] args) {
+                int upperLimit = 10;
+                for (int i = 2; i <= upperLimit; i++) {
+                    if (i % 2 == 0) {
+                        System.out.println(i);
+                    }
+                }
+            }
+        }
