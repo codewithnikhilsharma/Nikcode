19e4c645718b187d6d3b66e94095bda13a53bd33
Nikhil Sharma <122785600+codewithnikhilsharma@users.noreply.github.com>
2023-02-16 14:22:35 +0530

    Add files via upload

---
+++ file 1/Forloop.java
@@ -0,0 +1,14 @@
+package controlStatement;
+
+public class Forloop {
+	
+	public static void main(String[] args) {
+		
+		for(int i = 1; i<=10; i++) {
+			for(int j = 10; j<=20; j++) {
+			
+			System.out.println( i + " " + j);
+		}
+	}
+
+}}
