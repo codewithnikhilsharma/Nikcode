f096e66fb5b795a62612c9b5de33d17b02830594
Nikhil Sharma <122785600+codewithnikhilsharma@users.noreply.github.com>
2023-02-18 20:22:39 +0530

    Add files via upload

---
+++ Example.java
@@ -0,0 +1,17 @@
+package Now;
+
+	  
+public class Example {
+	  public static void main(String[] args) {
+	    int a = 5;
+	    int b = 10;
+	    int sum = addNumbers(a, b);
+	    System.out.println("The sum of " + a + " and " + b + " is " + sum);
+	  }
+
+	  public static int addNumbers(int x, int y) {
+	    int result = x + y;
+	    return result;
+	  }
+	}
+
