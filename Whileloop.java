af8a0bc55fbffb08dfc3adce2d5b630a9a560843
Nikhil Sharma <122785600+codewithnikhilsharma@users.noreply.github.com>
2023-02-19 21:28:01 +0530

    Add files via upload

---
+++ Whileloop.java
@@ -0,0 +1,18 @@
+package controlStatement;
+
+public class Whileloop {
+   
+	public static void main(String[] args) {
+	
+			    int i = 1;
+			    do {
+			      if (i % 2 != 0) {
+			        System.out.println(i);
+			      }
+			      i++;
+			    } while (i <= 20);
+	         }
+		 }
+			
+	
+
