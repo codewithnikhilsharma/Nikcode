3f3b15f0c492bc935c4906c0c54350f5be3886e3
Nikhil Sharma <122785600+codewithnikhilsharma@users.noreply.github.com>
2023-02-19 21:30:07 +0530

    Add files via upload

---
+++ Space.java
@@ -0,0 +1,18 @@
+package Object;
+
+public class Space {
+  
+	public static void main(String[] args) {
+		
+		
+			    int num = 123456;
+			    while (num > 0) {
+			      int digit = num % 10;
+			      System.out.println(digit + " ");
+			      num /= 10;
+			    }
+			  }
+			}
+
+	
+
