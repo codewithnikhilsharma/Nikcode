0a1a0d1731e3f675f258d8adae252b25a4e3c9ce
Nikhil Sharma <122785600+codewithnikhilsharma@users.noreply.github.com>
2023-03-05 23:12:21 +0530

    Add files via upload

---
+++ Postivevalue.java
@@ -0,0 +1,18 @@
+package Array;
+
+public class Postivevalue {
+
+	public static void main(String[] args) {
+		
+		
+		int[] dogo = {1, 5 ,8 , -2  };
+		
+		for( int val : dogo) {
+			if(val>=0) {
+				System.out.println(val);
+			}
+		}
+		
+		
+	}
+}
