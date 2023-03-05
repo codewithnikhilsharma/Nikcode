68f01ef1a0b21861afbb9ed1165c97d1f261f6b5
Nikhil Sharma <122785600+codewithnikhilsharma@users.noreply.github.com>
2023-03-05 22:52:43 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,15 @@
+package Array;
+
+public class Add {
+public static void main(String[] args) {
+	
+	int[] arr = { 1 , 5 , 3 , 4};
+	
+	int sum = 0;
+	for(int i = 0; i<arr.length; i++) {
+		sum+=arr[i];
+	}
+	
+	System.out.println(sum);
+}
+}
