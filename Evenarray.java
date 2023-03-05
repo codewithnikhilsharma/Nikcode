108896bf8bcbdbc075e409b7e9dd187bf5accf68
Nikhil Sharma <122785600+codewithnikhilsharma@users.noreply.github.com>
2023-03-05 23:47:23 +0530

    Add files via upload

---
+++ Evenarray.java
@@ -0,0 +1,14 @@
+package Array;
+
+public class Evenarray {
+
+	public static void main(String[] args) {
+		
+		int [] so = {1, 33, 334, 4,3, 3,34,34,3};
+		for(int val:so) {
+			if(val % 2 == 0 ) {
+				System.out.println(val);
+			}
+		}
+	}
+}
