ebe2a46effabd218ddb256857f422f53348d38c2
Nikhil Sharma <122785600+codewithnikhilsharma@users.noreply.github.com>
2023-03-08 17:19:09 +0530

    Add files via upload

---
+++ Exc.java
@@ -0,0 +1,16 @@
+package Now;
+
+public class Exc {
+	 public static void main(String[] args) {
+	        int x = 5;
+	        int y = 3;
+	        int sum = x + y;
+	        int difference = x - y;
+	        int product = x * y;
+	        double quotient = (double) x / y;
+	        System.out.println("The sum is " + sum);
+	        System.out.println("The difference is " + difference);
+	        System.out.println("The product is " + product);
+	        System.out.println("The quotient is " + quotient);
+	    }
+}
