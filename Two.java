e53d2d7e3fc6418637733da31039197c4bff200a
Nikhil Sharma <122785600+codewithnikhilsharma@users.noreply.github.com>
2023-03-10 16:24:28 +0530

    Add files via upload

---
+++ Two.java
@@ -0,0 +1,14 @@
+package Oops;
+
+public class Two {
+	    public static void main(String[] args) {
+	        int num = 7;
+	        if (num % 2 == 0) {
+	            System.out.println(num + " is even.");
+	        } else {
+	            System.out.println(num + " is odd.");
+	        }
+	    }
+	}
+
+
