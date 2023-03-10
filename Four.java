321149216a55aa28923ecbfb86a4091b23f86d73
Nikhil Sharma <122785600+codewithnikhilsharma@users.noreply.github.com>
2023-03-10 16:24:57 +0530

    Add files via upload

---
+++ Four.java
@@ -0,0 +1,16 @@
+package Oops;
+
+public class Four {
+	    public static void main(String[] args) {
+	        int num1 = 10, num2 = 20, num3 = 30;
+	        if (num1 > num2 && num1 > num3) {
+	            System.out.println(num1 + " is the largest number.");
+	        } else if (num2 > num1 && num2 > num3) {
+	            System.out.println(num2 + " is the largest number.");
+	        } else {
+	            System.out.println(num3 + " is the largest number.");
+	        }
+	    }
+	}
+
+
