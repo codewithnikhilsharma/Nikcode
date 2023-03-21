821f6951b87f9cdbe3e7fbc6bd9d89b313c2cdeb
Nikhil Sharma <122785600+codewithnikhilsharma@users.noreply.github.com>
2023-03-21 13:28:48 +0530

    Add files via upload

---
+++ Looping.java
@@ -0,0 +1,12 @@
+package JavaBasic;
+
+public class Looping {
+	    public static void main(String[] args) {
+	        int num = 5;
+	        for (int i = 1; i <= num; i++) {
+	            System.out.println(i);
+	        }
+	    }
+	}
+
+
