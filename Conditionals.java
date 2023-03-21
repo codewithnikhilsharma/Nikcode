fe3198e4edce804b74c988823d097422fc60799d
Nikhil Sharma <122785600+codewithnikhilsharma@users.noreply.github.com>
2023-03-21 13:28:22 +0530

    Add files via upload

---
+++ Conditionals.java
@@ -0,0 +1,17 @@
+package JavaBasic;
+
+public class Conditionals {
+
+	    public static void main(String[] args) {
+	        int num = 5;
+	        if (num > 0) {
+	            System.out.println("Positive");
+	        } else if (num < 0) {
+	            System.out.println("Negative");
+	        } else {
+	            System.out.println("Zero");
+	        }
+	    }
+	}
+
+
