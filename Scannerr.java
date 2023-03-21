072041e6de46c5e9f5d1061e478eb9fce2f9c410
Nikhil Sharma <122785600+codewithnikhilsharma@users.noreply.github.com>
2023-03-21 13:28:58 +0530

    Add files via upload

---
+++ Scannerr.java
@@ -0,0 +1,15 @@
+package JavaBasic;
+
+import java.util.Scanner;
+
+public class Scannerr {
+
+	    public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        System.out.print("Enter your name: ");
+	        String name = sc.nextLine();
+	        System.out.println("Hello, " + name + "!");
+	    }
+	}
+
+
