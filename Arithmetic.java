07fc5f5b57e483776467452797d501212474c401
Nikhil Sharma <122785600+codewithnikhilsharma@users.noreply.github.com>
2023-03-21 13:27:39 +0530

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,15 @@
+package JavaBasic;
+
+public class Arithmetic {
+	    public static void main(String[] args) {
+	        int num1 = 10;
+	        int num2 = 5;
+	        System.out.println("Sum: " + (num1 + num2));
+	        System.out.println("Difference: " + (num1 - num2));
+	        System.out.println("Product: " + (num1 * num2));
+	        System.out.println("Quotient: " + (num1 / num2));
+	        System.out.println("Remainder: " + (num1 % num2));
+	    }
+	}
+
+
