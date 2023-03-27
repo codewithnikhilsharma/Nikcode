d27d89ea18647ace3fd25b5350db2db05bcfbd6f
Nikhil Sharma <122785600+codewithnikhilsharma@users.noreply.github.com>
2023-03-27 13:57:04 +0530

    Update Program.java

--- Program.java
+++ Program.java
@@ -4,9 +4,9 @@ public class Greeting {
    public static void main(String[] args) {
       Scanner scanner = new Scanner(System.in);
       
-      System.out.print("Enter your name: ");
+      System.out.print(" Enter your full name: ");
       String name = scanner.nextLine();
       
-      System.out.println("Hello, " + name + "!");
+      System.out.println(" Hello, " + name + "!");
    }
 }
